bb8bf68ebeff78dee4c62fe400bfc1d68162ed20
Jason Green <hyx0112@yeah.net>
2017-07-01 15:25:17 +0800

    update

---
+++ Practices/src/Jason_test0701/test006.java
@@ -0,0 +1,27 @@
+package Jason_test0701;
+
+import java.awt.List;
+
+public class test006<T> {
+	T field;
+	public T getField() {
+		return field;
+	}
+	public void setField(T field) {
+		this.field = field;
+	}
+	public void whatIsT(){
+		System.out.println(field.getClass().getName());
+	}
+
+
+	public static void main(String[] args) {
+		test006<String> t06 = new test006<String>();
+		t06.setField("this is my practice.");
+		t06.whatIsT();
+	}
+	
+
+}
+
+
---
+++ Practices/src/Jason_test0701/test007.java
@@ -0,0 +1,9 @@
+package Jason_test0701;
+import java.util.ArrayList;
+
+public class test007{
+	public static void main(String[] args) {
+
+	}
+	
+}
